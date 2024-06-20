4ac9fd335bb87bb5ea98d24e7fa9e5b2e052e0fa
Mennatall-Khougha <menmen.khougha@gmail.com>
2024-06-20 13:35:42 +0300

    remove react

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import api from '../api'
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
